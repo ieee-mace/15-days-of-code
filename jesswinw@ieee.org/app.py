78692e4b97a05b4a2a848eefe1b6ba7c53457f24
jesswinw <jesswinw@gmail.com>
2020-05-11 23:15:18 +0530

    added template support

--- jesswinw@ieee.org/app.py
+++ jesswinw@ieee.org/app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [{
+    'author': 'Bob Vance',
+    'title': 'Refridgerators affecting my Life',
+    'content': 'asdhaskdhaskjhd daskjdhkjash kdjsah kdjhaskjdh kash dkjash dkjash k',
+    'date': 'April 23,2020'
+}, {
+    'author': 'John Doe',
+    'title': 'Everyody uses my name!!',
+    'content': 'asdhaskdhaskjhd daskjdhkjash kdjsah kdjhaskjdh kash dkjash dkjash k',
+    'date': 'April 22,2020'
+}]
 
 
 @app.route('/')
 @app.route('/home')
 def index():
-    return "<h1>Hello World</h1>"
+    return render_template('index.html', posts=posts)
 
 
 @app.route('/about')
