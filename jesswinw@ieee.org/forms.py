04e93e4b275d4eed5033ada63d34ad2403f94b3c
jesswinw <jesswinw@gmail.com>
2020-05-12 23:12:00 +0530

    forms.py to manage forms😉

---
+++ jesswinw@ieee.org/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=2, max=16)])
+    email = StringField('E-Mail', validators=[DataRequired(), Email()])
+
+    password = PasswordField('password', validators=[DataRequired()])
+    con_password = PasswordField('con_password', validators=[
+                                 DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('E-Mail', validators=[DataRequired(), Email()])
+
+    password = PasswordField('password', validators=[DataRequired()])
+
+    remember = BooleanField('Remember Me!')
+    submit = SubmitField('Log In')
