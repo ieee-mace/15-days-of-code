c60597f49bb08cdda0eeed87a2dcf798cbc5d245
annu12340 <annujolly17@gmail.com>
2020-05-12 21:34:13 +0530

    adding react files

---
+++ annujolly17@gmail.com/blog_post/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Footer() {
+    return (
+      <div className="footer">
+       footer
+        
+      </div>
+    );
+  }
+  
+  export default Footer;
+  
---
+++ annujolly17@gmail.com/blog_post/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+      <div className="navbar">
+        <ul>
+           Navbar
+        </ul>
+        
+      </div>
+    );
+  }
+  
+  export default Navbar;
+  
