3396dcfa690d73501e611d7477f24da845fc3ac3
annu12340 <annujolly17@gmail.com>
2020-05-13 21:59:58 +0530

    form component

---
+++ annujolly17@gmail.com/blog_post/src/components/Form.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+
+class Form extends Component() {
+    render(){
+        return (
+            <div className="navbar">
+              <ul>
+                 Navbar
+              </ul>
+              
+            </div>
+          );
+    }
+  
+  }
+  
+  export default Form;
+  
