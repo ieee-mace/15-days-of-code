8ebfed558623b1df610acae87017c0c10e21f4d3
Annu <annujolly17@gmail.com>
2020-05-13 22:30:45 +0530

    Update Form.js

--- annujolly17@gmail.com/blog_post/src/components/Form.js
+++ annujolly17@gmail.com/blog_post/src/components/Form.js
@@ -3,9 +3,10 @@ import React,{Component} from 'react';
 class Form extends Component() {
     render(){
         return (
-            <div className="form">
-              input 
-              
+             <div className="form">
+              <input placeholder="enter title"/>
+              <input placeholder="enter post"/>
+              <button>Submit</button>
             </div>
           );
     }
