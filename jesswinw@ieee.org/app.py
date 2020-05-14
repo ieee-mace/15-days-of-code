83c775a9f7f9b9497914a2a81a18cf1cdd977b7e
jesswinw <jesswinw@gmail.com>
2020-05-14 23:52:48 +0530

    added redirect feature

--- jesswinw@ieee.org/app.py
+++ jesswinw@ieee.org/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -26,6 +26,10 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def reg():
     form = RegistrationForm()
+    if(form.validate_on_submit()):
+        flash(
+            f'Account successfully created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
 
 
