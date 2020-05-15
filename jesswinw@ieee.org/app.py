be93dd5cea2769e4ca982cef1813c6a65c68b1fb
jesswinw <jesswinw@gmail.com>
2020-05-15 14:48:14 +0530

    some minor changes

--- jesswinw@ieee.org/app.py
+++ jesswinw@ieee.org/app.py
@@ -33,10 +33,17 @@ def reg():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('register.html', title='Login', form=form)
+    if(form.validate_on_submit()):
+        if(form.email.data == 'admin@bloggy.com' and form.password.data == 'password'):
+            flash('You have been logged In', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Incorrect Password or Email ID', 'danger')
+
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == "__main__":
