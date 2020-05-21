b46fcb116090165ea21ac7984e19dfca710e66db
jesswinw <jesswinw@gmail.com>
2020-05-21 18:31:33 +0530

    Added post deletion feature🤩

--- jesswinw@ieee.org/bloggy/routes.py
+++ jesswinw@ieee.org/bloggy/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, redirect, flash,request
+from flask import render_template, url_for, redirect, flash,request, abort
 from bloggy import app, bcrypt, db
 from bloggy.forms import RegistrationForm, LoginForm, UserupdateForm, PostForm
 from datetime import datetime
@@ -92,4 +92,39 @@ def create_post():
         db.session.commit()
         flash("Your post has been created",'success')
         return redirect(url_for('index'))
-    return render_template('create_post.html',title='New Post',form=form)
+    return render_template('create_post.html',title='New Post',form=form,legend="Create Post")
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Posts.query.get_or_404(post_id)
+    return render_template('post.html',title=post.title,post=post)
+
+@app.route('/post/<int:post_id>/update',methods=['GET','POST'])
+@login_required
+def update_post(post_id):
+    post = Posts.query.get_or_404(post_id)
+    if(post.author!=current_user):
+        abort(403)
+    form = PostForm()
+    if(form.validate_on_submit()):
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated','success')
+        return redirect(url_for('post',post_id=post.id))
+    elif(request.method == 'GET'):
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html',title='Update Post',form=form,legend="Update Post")
+
+@app.route('/post/<int:post_id>/delete',methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Posts.query.get_or_404(post_id)
+    if(post.author!=current_user):
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted','success')
+    return redirect(url_for('index'))
+    
