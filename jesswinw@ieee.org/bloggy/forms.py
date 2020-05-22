3561ae98d1f8ffe60bc50c140a0ca1c4898280a2
jesswinw <jesswinw@gmail.com>
2020-05-22 23:16:20 +0530

    added some constraints for the username

--- jesswinw@ieee.org/bloggy/forms.py
+++ jesswinw@ieee.org/bloggy/forms.py
@@ -1,13 +1,12 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField,FileAllowed 
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Regexp
 from bloggy.models import Users, Posts
 from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[
-                           DataRequired(), Length(min=2, max=16)])
+    username = StringField('Username', validators=[DataRequired(), Regexp('^\w+$',message="Username should only contain alphanumeric characters and _s"), Length(min=2, max=16)])
     email = StringField('E-Mail', validators=[DataRequired(), Email()])
 
     password = PasswordField('Password', validators=[DataRequired()])
